1869fbcb8f758eca595da1432b811e9c16b2f0a4
Melissa Robbins <melissamrobbins@gmail.com>
2019-11-01 17:42:33 -0600

    Add Enigma test file

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,1 +1,14 @@
-# test file for stuff
+require_relative './test_helper'
+require './lib/enigma'
+
+class EnigmaTest < Minitest::Test
+
+  def setup
+    @enigma = Enigma(arg1, arg2)
+  end
+
+  def test_it_exists
+    assert_instance_of Enigma, @enigma
+  end
+
+end
