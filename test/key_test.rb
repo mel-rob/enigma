16347f1db24b7ae184dc2408eef5cf4990a2d4b4
Melissa Robbins <melissamrobbins@gmail.com>
2019-11-01 19:09:15 -0600

    Add generate_keys method test and helper method tests

--- test/key_test.rb
+++ test/key_test.rb
@@ -16,4 +16,17 @@ class KeyTest < Minitest::Test
     assert_equal 37834, @key.random_number
   end
 
+  def test_it_converts_number_to_string
+    assert_equal 5, @key.convert_number_to_string.length
+    assert_equal "12345", @key.convert_number_to_string
+  end
+
+  def test_it_converts_string_to_array
+    assert_equal ["1", "2", "3", "4", "5"], @key.convert_string_to_array
+  end
+
+  def test_generate_keys
+    assert_equal ["12", "23", "34", "45"], @key.generate_keys
+  end
+
 end
