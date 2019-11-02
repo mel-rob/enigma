b37305cdec903b5998f82943699dd5f9bd565802
Melissa Robbins <melissamrobbins@gmail.com>
2019-11-01 19:08:38 -0600

    Add generate_keys method and helper methods

--- lib/key.rb
+++ lib/key.rb
@@ -6,4 +6,18 @@ class Key
     @random_number = rand(99999)
   end
 
+  def convert_number_to_string
+    @random_number.to_s.rjust(5, "0")
+  end
+
+  def convert_string_to_array
+    convert_number_to_string.chars
+  end
+
+  def generate_keys
+    keys = []
+    convert_string_to_array.each_cons(2) {|number| keys << number }
+    keys.map { |key| key.join }
+  end
+
 end
