bace82847121f99a9a0442c4fdd9cb9c25a3e7bd
Melissa Robbins <melissamrobbins@gmail.com>
2019-11-01 21:31:36 -0600

    Add generate_offsets tests with helper method tests

--- test/offset_test.rb
+++ test/offset_test.rb
@@ -15,4 +15,21 @@ class OffsetTest < Minitest::Test
   def test_it_initializes_with_todays_date
     assert_equal '011119', @offset.date
   end
+
+  def test_it_can_square_date
+    assert_equal 123632161, @offset.square_date
+  end
+
+  def test_it_can_convert_number_to_string
+    assert_equal "123632161", @offset.convert_number_to_string
+  end
+
+  def test_it_can_get_last_four_characters
+    assert_equal "2161", @offset.get_last_four_characters
+  end
+
+  def test_it_can_generate_offsets
+    assert_equal ["2", "1", "6", "1"], @offset.generate_offsets
+  end
+
 end
