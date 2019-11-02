f648be4abed54aaebabe9550ebd72cbcad58fd70
Melissa Robbins <melissamrobbins@gmail.com>
2019-11-01 22:45:05 -0600

    Change generate_keys method to deliver integers

--- lib/key.rb
+++ lib/key.rb
@@ -11,13 +11,13 @@ class Key
   end
 
   def convert_string_to_array
-    convert_number_to_string.chars
+    convert_number_to_string.each_char.map { |c| Integer(c) }
   end
 
   def generate_keys
     keys = []
-    convert_string_to_array.each_cons(2) {|number| keys << number }
-    keys.map { |key| key.join }
+    convert_string_to_array.each_cons(2).map {|number| keys << number }
+    a = keys.map { |key| key.join.to_i }
   end
 
 end
