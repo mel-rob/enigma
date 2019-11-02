11e64e136fbdde15de8517bbba9cbfddce066b6b
Melissa Robbins <melissamrobbins@gmail.com>
2019-11-02 17:42:06 -0600

    Refactor Key class

--- lib/key.rb
+++ lib/key.rb
@@ -1,23 +1,7 @@
 class Key
 
-  attr_reader :random_number
-
-  def initialize
-    @random_number = rand(99999)
-  end
-
-  def convert_number_to_string
-    @random_number.to_s.rjust(5, "0")
-  end
-
-  def convert_string_to_array
-    convert_number_to_string.each_char.map { |char| Integer(char) }
-  end
-
-  def generate_keys
-    keys = []
-    convert_string_to_array.each_cons(2).map {|number| keys << number }
-    a = keys.map { |key| key.join.to_i }
+  def self.get_number
+    rand.to_s[2..6]
   end
 
 end
