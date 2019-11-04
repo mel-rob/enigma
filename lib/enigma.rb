396cbedaeee17d113fef77a87f780ba45650d29f
Melissa Robbins <melissamrobbins@gmail.com>
2019-11-04 11:53:39 -0700

    Add decrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,26 +2,40 @@ require_relative 'date'
 require_relative 'key'
 require_relative 'shift'
 require_relative 'encrypt'
+require_relative 'decrypt'
 
-  class Enigma
+class Enigma
 
-    attr_reader :date, :key
+  # attr_reader :date, :key
 
-    def initialize
-      @date = Date.get_date
-      @key = Key.get_number
-      @encrypt = Encrypt.new
-    end
-
-    def encrypt(message, key = nil, date = nil)
-      key = @key if key == nil || key.class == Integer || key.length != 5
-      date = @date if date == nil || date.class == Integer || date.length != 6
-
-      {
-        encryption: @encrypt.message_shift(message, key, date),
-        key: key,
-        date: date
-      }
-
-    end
+  def initialize
+    @date = Date.get_date
+    @key = Key.get_number
+    @encrypt = Encrypt.new
+    @decrypt = Decrypt.new
   end
+
+  def encrypt(message, key = nil, date = nil)
+    # key = @key if key == nil || key.class == Integer || key.length != 5
+    key = @key if key == nil || key.count("0123456789") != 5
+    # date = @date if date == nil || date.class == Integer || date.length != 6
+    date = @date if date == nil || date.count("0123456789") != 6
+    # date = @date if date == nil || date.class == Integer || date.length != 6
+    {
+      encryption: @encrypt.message_shift(message, key, date),
+      key: key,
+      date: date
+    }
+  end
+
+  def decrypt(message, key, date = nil)
+    date = @date if date == nil || date.class == Integer || date.length != 6
+    # use same logic above or add into CLI class?
+    {
+      decryption: @decrypt.message_shift(message, key, date),
+      key: key,
+      date: date
+    }
+  end
+
+end
