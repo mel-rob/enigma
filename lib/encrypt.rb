216ea2a475a1d7865424aa29727f65c8d50204b5
Melissa Robbins <melissamrobbins@gmail.com>
2019-11-05 16:25:58 -0700

    Remove comments in Encrypt class

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -15,20 +15,4 @@ class Encrypt
       value
     end.join
   end
-
-  # def message_shift(message, key, date)
-  #   shift_values = Shift.generate_shift(key, date)
-  #   output = message.downcase.chars.map do |letter|
-  #     shift = shift_values[0]
-  #     shifted = @alphabet.rotate(shift)
-  #       if !@alphabet.include?(letter)
-  #         letter = letter
-  #       else
-  #         letter.replace(shifted[@alphabet.index(letter)])
-  #       end
-  #     shift_values.rotate!
-  #     letter
-  #   end.join
-  # end
-
 end
